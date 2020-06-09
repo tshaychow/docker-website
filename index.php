a4ea6571195b38ed05c11250320ee9b0ecd24259
Eric Chow <TsEric@tshay.fritz.box>
2020-06-09 17:51:13 +0200

    ingrid

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <h2> 
             <?php   
                 $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, 'http://192.168.64.2:32325/');
+                curl_setopt($ch, CURLOPT_URL, 'ser-service:32000');
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 $output = curl_exec($ch);
                 curl_close($ch);     
