201ce54cbcdf532bf9ab49af03b9cb6ea3cdfaab
Eric Chow <TsEric@tshay.fritz.box>
2020-06-09 13:56:33 +0200

    link

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <h2> 
             <?php   
-                $json = file_get_contents('http://ser-services');
+                $json = file_get_contents('ser-services');
                 $obj = json_decode($json);
                 $amount = $obj->output;
                 echo  $amount;
